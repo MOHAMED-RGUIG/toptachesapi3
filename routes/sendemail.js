86bbe0e34cab0c062bab6fb5c7682bebdcb5591b
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-10-07 13:05:09 +0200

    Update sendemail.js

--- routes/sendemail.js
+++ routes/sendemail.js
@@ -185,7 +185,7 @@ const transporter = nodemailer.createTransport({
     const doc = new jsPDF();
   
     // Adding a logo
-    const logoBase64 = getImageBase64('https://topclassgestiontaches.app.genez.io/public/img/logo_footer.png');
+    const logoBase64 = getImageBase64('./logo_footer.png');
     doc.addImage(logoBase64, 'PNG', 10, 13, 37, 27);
   
     // Document Title
