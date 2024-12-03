bd90883a97bb09c52e76d947a132c9126060fae0
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-03 11:29:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin:["https://topclass-expresso.vercel.app/"],
+  origin:["https://topclass-expresso.vercel.app"],
   methods:["POST","GET","DELETE","PUT"],
   credentials:true}));
 // Use the auth routes
