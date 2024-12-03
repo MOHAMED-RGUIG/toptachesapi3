7120b78a64dfc61720087af347d4a7045ef5778b
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-03 12:03:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin:["https://topclassgstc.app.genez.io/"],
+  origin:["https://topclassexpresso.onrender.com/"],
   methods:["POST","GET","DELETE","PUT"],
   credentials:true}));
 // Use the auth routes
