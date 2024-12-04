cfc2bdaf9b5868c78b20c074bd4eac7740c7ad86
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-04 11:59:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import sql from './database.js'; 
@@ -62,4 +62,57 @@ app.get("/", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});*/
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import sql from './database.js'; 
+import authRoutes from './routes/auth.js'; 
+import userRoutes from './routes/user.js'; 
+import updateUserRoutes from './routes/updateuser.js';
+import addTaskRoutes from './routes/addtask.js';
+import tasksAllRoutes from './routes/tasksall.js';
+import updateTaskRoutes from './routes/updatetask.js';
+import taskByNumRoutes from './routes/taskbynum.js'; 
+import deleteTaskRoutes from './routes/deletetask.js'; 
+import notificationRoutes from './routes/notification.js';
+import sendEmailRoutes from './routes/sendemail.js';
+
+
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin:["https://topclassgstc.app.genez.io"],
+  methods:["POST","GET","DELETE","PUT"],
+  credentials:true}));
+// Use the auth routes
+app.use('/auth', authRoutes);
+
+// Use the user routes
+app.use('/user', userRoutes);
+
+// Use the updateUser routes
+app.use('/updateuser', updateUserRoutes);
+
+// Use the tasks routes
+app.use('/addtask', addTaskRoutes); 
+app.use('/tasks', taskByNumRoutes); 
+app.use('/tasks', tasksAllRoutes); 
+app.use('/tasks', updateTaskRoutes);  
+app.use('/tasks', deleteTaskRoutes);    
+
+app.use('/notifications', notificationRoutes);
+
+app.use('/api', sendEmailRoutes);
+app.get("/", async (req, res) => {
+  res.send("Server working!!!");
 });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
